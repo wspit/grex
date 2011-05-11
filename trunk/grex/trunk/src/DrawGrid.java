3ccecafb13355792014932acc2d176d74b405ff3
Waldemar.Spitschak <Waldemar.Spitschak@c6b32531-d14b-17c3-5dfe-6e0aa4862fda>
2011-05-11 16:32:57 +0000

    updated grid to fit the actual panel size

--- trunk/grex/trunk/src/DrawGrid.java
+++ trunk/grex/trunk/src/DrawGrid.java
@@ -2,37 +2,34 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
-import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 
 public class DrawGrid extends JPanel {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private int rows;
 	private int columns;
 	private int width;
 	private int height;
-//	private GridLayout layout;
-	
 
 	/**
 	 * Constructor for DrawGrid
-	 * @param n = Number of rows
-	 * @param m = Number of columns
-	 * @param width = width
-	 * @param height = height
+	 * 
+	 * @param n
+	 *            = Number of rows
+	 * @param m
+	 *            = Number of columns
+	 * @param width
+	 *            = width
+	 * @param height
+	 *            = height
 	 */
 	public DrawGrid(int n, int m, int width, int height) {
 		this.rows = n;
 		this.columns = m;
-		this.height = height;		
+		this.height = height;
 		this.width = width;
-//		layout = new GridLayout(10,10);
-//		setLayout(layout);
-		
+
 	}
 
 	public Dimension getPreferredSize() {
@@ -40,8 +37,7 @@ public class DrawGrid extends JPanel {
 	}
 
 	protected void paintComponent(Graphics g) {
-		
-		
+
 		g.setColor(Color.black);
 		int columnSize = 0;
 		int rowSize = 0;
@@ -53,24 +49,12 @@ public class DrawGrid extends JPanel {
 			g.drawLine(0, rowSize, getWidth(), rowSize);
 			rowSize = spaceBetweenRows + rowSize;
 		}
-		g.drawLine(0, rowSize-1, getWidth()-1, rowSize-1);
 
 		for (int column = 0; column < columns; column++) {
-			g.drawLine(columnSize, 0, columnSize,getHeight());
+			g.drawLine(columnSize, 0, columnSize, getHeight());
 			columnSize = spaceBetweenColumns + columnSize;
 		}
-		g.drawLine(columnSize-1, 0, getWidth()-1, columnSize-1 );
-		
-		
+
 	}
-	
-	
-			
-	
-	
-	
-	
-	
-	
 
 }
--- trunk/grex/trunk/src/GridActor.java
+++ trunk/grex/trunk/src/GridActor.java
@@ -1,13 +1,7 @@
-import java.awt.FlowLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-
-import greenfoot.World;
 import greenfoot.core.WorldHandler;
-import greenfoot.gui.ControlPanel;
-import greenfoot.platforms.standalone.SimulationDelegateStandAlone;
 
-import javax.swing.JCheckBox;
+import java.awt.FlowLayout;
+
 import javax.swing.JPanel;
 
 public class GridActor {
@@ -20,64 +14,22 @@ public class GridActor {
 	public GridActor(int width, int height, int nrOfColumns, int nrOfRows) {
 		gridWidth = width;
 		gridHeight = height;
-		// setImage(new GreenfootImage(gridWidth, gridHeight));
 		this.nrOfColumns = nrOfColumns;
 		this.nrOfRows = nrOfRows;
 
 		paint(this.nrOfColumns, this.nrOfRows);
 	}
 
-	// public void paint(int nrOfColumns, int nrOfRows) {
-	// lock();
-
-	// GreenfootImage lines = getImage();
-	// int spaceBetweenColumns = lines.getHeight() / nrOfColumns;
-	// int spaceBetweenRows = lines.getWidth() / nrOfRows;
-	// int columnSize = 0;
-	// int rowSize = 0;
-	//
-	// for (int rows = 0; rows < nrOfRows; rows++) {
-	// lines.drawLine(rowSize, 0, rowSize, gridHeight);
-	// rowSize = spaceBetweenRows + rowSize;
-	// }
-	// lines.drawLine(rowSize - 1, 0, rowSize - 1, gridHeight);
-	//
-	// for (int columns = 0; columns < nrOfColumns; columns++) {
-	// lines.drawLine(0, columnSize, gridWidth, columnSize);
-	// columnSize = spaceBetweenColumns + columnSize;
-	//
-	// }
-	// lines.drawLine(0, columnSize - 1, gridWidth, columnSize - 1);
-
-	// lockTest();
-	// }
-
 	public void paint(int nrOfColumns, int nrOfRows) {
 		JPanel panel = WorldHandler.getInstance().getWorldCanvas();
-//		panel.setLayout(new GridLayout(nrOfRows, nrOfColumns));
-		DrawGrid grid = new DrawGrid(nrOfRows, nrOfColumns, gridWidth, gridHeight);
+		DrawGrid grid = new DrawGrid(nrOfRows, nrOfColumns, gridWidth,
+				gridHeight);
+		FlowLayout lm = (FlowLayout) panel.getLayout();
+		lm.setVgap(0);		
 		panel.add(grid);
-		
 	
-//		showCheckBox(panel);
-	}
-	
-		private void showCheckBox(JPanel panel){		
-//			JCheckBox box = new JCheckBox("show grid");		
-//			ControlPanel cpanel = new ControlPanel(new SimulationDelegateStandAlone(), true);
-//			panel.add(box);
-
-//			box.setSelected(true);
-			
-//			if (box.isSelected()){
-//				panel.getComponent(0).setVisible(true);
-//			}  else {
-//				panel.getComponent(0).setVisible(false); 
-//			}
 		
-		}	
-
-
+		
+		
 	}
-
-
+}
